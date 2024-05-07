d12fa130d972a1296e37fbb80c60dbb2e4d6ef3c
notlearnedd <153484430+notlearnedd@users.noreply.github.com>
2024-05-07 18:34:06 +0800

    training_script update

--- training_script.py
+++ training_script.py
@@ -100,7 +100,7 @@ class Parameters:
         if self.SAVE_DIR is None:
             self.SAVE_DIR: str = str(pathlib.Path.cwd())
 
-
+'''
 def train():
     # environment variables (pydantic BaseSettings class)
     neptune_settings = NeptuneSettings()
@@ -302,7 +302,215 @@ def train():
     # stop logger
     neptune_logger.experiment.stop()
     logger.info("Training finished")
+'''
 
+# KFOLD MODIFIED
 
+def train():
+    # environment variables (pydantic BaseSettings class)
+    neptune_settings = NeptuneSettings()
+
+    # parameters (dataclass)
+    parameters = Parameters()
+
+    # data path relative to this file (pathlib)
+    data_path: pathlib.Path = (
+        ROOT_PATH / "pytorch_faster_rcnn_tutorial" / "data" / "heads"
+    )
+
+    # input and target files
+    inputs: List[pathlib.Path] = []
+    targets: List[pathlib.Path] = []
+    for fold in range(5):
+        fold_path = pathlib.Path("split_data") / f"fold{fold}"
+        inputs.extend(get_filenames_of_path(fold_path / "JPEG"))
+        targets.extend(get_filenames_of_path(fold_path / "Annotations"))
+
+    # sort inputs and targets
+    inputs.sort()
+    targets.sort()
+
+    # mapping
+    mapping: Dict[str, int] = {"head": 1}
+
+    # training transformations and augmentations
+    transforms_training = ComposeDouble(
+        [
+            Clip(),
+            AlbumentationWrapper(albumentations.HorizontalFlip(p=0.5)),
+            AlbumentationWrapper(
+                albumentations.RandomScale(p=0.5, scale_limit=0.5)
+            ),
+            # AlbuWrapper(albu=A.VerticalFlip(p=0.5)),
+            FunctionWrapperDouble(function=np.moveaxis, source=-1, destination=0),
+            FunctionWrapperDouble(function=normalize_01),
+        ]
+    )
+
+    # validation transformations
+    transforms_validation = ComposeDouble(
+        [
+            Clip(),
+            FunctionWrapperDouble(function=np.moveaxis, source=-1, destination=0),
+            FunctionWrapperDouble(function=normalize_01),
+        ]
+    )
+
+    # test transformations
+    transforms_test = ComposeDouble(
+        [
+            Clip(),
+            FunctionWrapperDouble(function=np.moveaxis, source=-1, destination=0),
+            FunctionWrapperDouble(function=normalize_01),
+        ]
+    )
+
+    # random seed (function that sets seed for pseudo-random number generators in: pytorch, numpy, python.random)
+    seed_everything(parameters.SEED)
+
+    # training validation test split (manually)
+    inputs_train, inputs_valid, inputs_test = inputs[:12], inputs[12:16], inputs[16:]
+    targets_train, targets_valid, targets_test = (
+        targets[:12],
+        targets[12:16],
+        targets[16:],
+    )
+
+    # dataset training
+    dataset_train = ObjectDetectionDataSet(
+        inputs=inputs_train,
+        targets=targets_train,
+        transform=transforms_training,
+        use_cache=parameters.CACHE,
+        convert_to_format=None,
+        mapping=mapping,
+    )
+
+    # dataset validation
+    dataset_valid = ObjectDetectionDataSet(
+        inputs=inputs_valid,
+        targets=targets_valid,
+        transform=transforms_validation,
+        use_cache=parameters.CACHE,
+        convert_to_format=None,
+        mapping=mapping,
+    )
+
+    # dataset test
+    dataset_test = ObjectDetectionDataSet(
+        inputs=inputs_test,
+        targets=targets_test,
+        transform=transforms_test,
+        use_cache=parameters.CACHE,
+        convert_to_format=None,
+        mapping=mapping,
+    )
+
+    # dataloader training
+    dataloader_train = DataLoader(
+        dataset=dataset_train,
+        batch_size=parameters.BATCH_SIZE,
+        shuffle=True,
+        num_workers=0,
+        collate_fn=collate_double,
+    )
+
+    # dataloader validation
+    dataloader_valid = DataLoader(
+        dataset=dataset_valid,
+        batch_size=1,
+        shuffle=False,
+        num_workers=0,
+        collate_fn=collate_double,
+    )
+
+    # dataloader test
+    dataloader_test = DataLoader(
+        dataset=dataset_test,
+        batch_size=1,
+        shuffle=False,
+        num_workers=0,
+        collate_fn=collate_double,
+    )
+
+    # neptune logger (neptune-client)
+    neptune_logger = NeptuneLogger(
+        api_key=neptune_settings.api_key,
+        project=f"{neptune_settings.OWNER}/{neptune_settings.PROJECT}",  # use your neptune name here
+        name=neptune_settings.PROJECT,
+        log_model_checkpoints=False,
+    )
+
+    # log hyperparameters
+    neptune_logger.log_hyperparams(asdict(parameters))
+
+    # model init
+    model: FasterRCNN = get_faster_rcnn_resnet(
+        num_classes=parameters.CLASSES,
+        backbone_name=parameters.BACKBONE,
+        anchor_size=parameters.ANCHOR_SIZE,
+        aspect_ratios=parameters.ASPECT_RATIOS,
+        fpn=parameters.FPN,
+        min_size=parameters.MIN_SIZE,
+        max_size=parameters.MAX_SIZE,
+    )
+
+    # lightning model
+    model: FasterRCNNLightning = FasterRCNNLightning(
+        model=model, lr=parameters.LR, iou_threshold=parameters.IOU_THRESHOLD
+    )
+
+    # callbacks
+    checkpoint_callback: ModelCheckpoint = ModelCheckpoint(
+        monitor="Validation_mAP", mode="max"
+    )
+    learning_rate_callback: LearningRateMonitor = LearningRateMonitor(
+        logging_interval="step", log_momentum=False
+    )
+    early_stopping_callback: EarlyStopping = EarlyStopping(
+        monitor="Validation_mAP", patience=parameters.PATIENCE, mode="max"
+    )
+
+    # trainer init
+    trainer: Trainer = Trainer(
+        accelerator=parameters.ACCELERATOR,
+        logger=neptune_logger,
+        callbacks=[
+            checkpoint_callback,
+            learning_rate_callback,
+            early_stopping_callback,
+        ],
+        default_root_dir=parameters.SAVE_DIR,  # where checkpoints are saved to
+        log_every_n_steps=1,  # increase to reduce the amount of log flushes (lowers the overhead)
+        num_sanity_val_steps=0,  # set to 0 to skip sanity check
+        max_epochs=parameters.MAXEPOCHS,
+        fast_dev_run=parameters.FAST_DEV_RUN,  # set to True to test the pipeline with one batch and without validation, testing and logging
+    )
+
+    # start training
+    trainer.fit(
+        model=model,
+        train_dataloaders=dataloader_train,
+        val_dataloaders=dataloader_valid,
+    )
+
+    if not parameters.FAST_DEV_RUN:
+        # start testing
+        trainer.test(ckpt_path="best", dataloaders=dataloader_test)
+
+        # log model
+        if parameters.LOG_MODEL:
+            checkpoint_path = pathlib.Path(checkpoint_callback.best_model_path)
+            log_model_neptune(
+                checkpoint_path=checkpoint_path,
+                save_directory=pathlib.Path.home(),
+                name="best_model.pt",
+                neptune_logger=neptune_logger,
+            )
+
+    # stop logger
+    neptune_logger.experiment.stop()
+    logger.info("Training finished")
+    
 if __name__ == "__main__":
     train()

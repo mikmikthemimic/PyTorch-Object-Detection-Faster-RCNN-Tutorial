d6d5b61a47ec59674f510f4f28fb294db7571f47
Abi M. <almanoguid@gmail.com>
2024-07-02 15:11:43 +0800

    hyperparameter tuning

--- pytorch_faster_rcnn_tutorial/data/heads/video.py
+++ pytorch_faster_rcnn_tutorial/data/heads/video.py
@@ -1,48 +1,85 @@
-import glob
-import json
 import cv2
+import json
 import math
+import glob
+import os
 
-images = glob.glob("input/*.jpg")
+import numpy as np
+import torch
+from torchvision.ops import nms
 
-annotations = glob.glob("target/*.json")
+predictions = glob.glob('predictions/*.json')
+images = glob.glob('test/*.jpg')
 
-video = cv2.VideoWriter('output.avi', cv2.VideoWriter_fourcc(*'DIVX'), 30, (1280, 720))
+labels = {
+    1: 'Vehicle',
+    2: 'Pedestrian',
+    3: 'Bicycle',
+    4: 'Motorcycle',
+    5: 'Tricycle',
+    99: 'Unknown'
+}
 
-for i in range(0, len(images)):
-    annotation = open(annotations[i], 'r')
-    image = cv2.imread(images[i])
-    data = json.load(annotation)
+IOU_THRESHOLD = 0.7
+SCORE_THRESHOLD = 0.55
 
-    image_name = images[i].split('\\')[1]
-    image_name = image_name.split('.')[0]
-    image_name = int(image_name)
-    print(image_name)
+USE_NMS = True
 
-    for k in range(0, len(data['labels'])):
-        label = data['labels'][k]
-        box = [math.trunc(float(i)) for i in data['boxes'][k]]
+video = cv2.VideoWriter('output.avi', cv2.VideoWriter_fourcc(*'XVID'), 10, (1280, 720))
+
+if not os.path.exists('output'):
+    os.makedirs('output')
+
+for i in range(len(predictions)):
+    filename = os.path.basename(images[i])
+    filename = filename.split('.')[0]
+
+    with open(predictions[i]) as f:
+        data = json.load(f)
+        image = cv2.imread(images[i])
+
+        nms_boxes = torch.tensor(data['boxes'])
+        if nms_boxes.size()[0] > 0 and USE_NMS:
+            nms_labels = torch.tensor(data['labels'])
+            nms_scores = torch.tensor(data['scores'])
+
+            mask = nms(nms_boxes, nms_scores, iou_threshold=IOU_THRESHOLD)
+            mask = (np.array(mask),)
+
+            data['boxes'] = np.asarray(nms_boxes)[mask]
+            data['labels'] = np.asarray(nms_labels)[mask]
+            data['scores'] = np.asarray(nms_scores)[mask]
+
+        for j in range(len(data['labels'])):
+            confidence = data['scores'][j]
+            if confidence < SCORE_THRESHOLD:
+                continue
+
+            label = data['labels'][j]
+            box = [math.trunc(float(i)) for i in data['boxes'][j]]
+
+            if label not in labels:
+                print(f'Found unknown label: {label} from {predictions[i]}')
+                label = 99
+
+            match labels[label]:
+                case 'Vehicle':
+                    bnd_color = (0, 0, 139)
+                    text_color = (0, 0, 255)
+                case 'Pedestrian':
+                    bnd_color = (0, 255, 0)
+                    text_color = (1, 250, 32)
+                case _:
+                    bnd_color = (255, 0, 0)
+                    text_color = (36, 255, 12)
+            
+            image = cv2.rectangle(image, (box[0], box[1]), (box[2], box[3]), bnd_color, 1)
+            cv2.putText(image, labels[label], (box[0], box[1] - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, text_color, 1)
+            cv2.putText(image, f'{confidence:.2f}', (box[0], box[1] - 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, text_color, 1)
+            cv2.putText(image, filename, (1150, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+            cv2.putText(image, ("NMS" if USE_NMS else "No NMS"), (1150, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, ((0, 255, 0) if USE_NMS else "No NMS"), 2)
         
-        min_x, min_y, max_x, max_y = box[0], box[1], box[2], box[3]
-
-        # switch case for different labels
-        match label:
-            case 'Vehicle':
-                bnd_color = (0, 0, 139)
-                text_color = (0, 0, 255)
-            case 'Pedestrian':
-                bnd_color = (0, 255, 0)
-                text_color = (1, 250, 32)
-            case _:
-                bnd_color = (255, 0, 0)
-                text_color = (36, 255, 12)
-
-        image = cv2.rectangle(image, (min_x, min_y), (max_x, max_y), bnd_color, 1)
-        cv2.putText(image, label, (min_x, min_y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, text_color, 1)
-
-        # put the frame number on the upper right corner
-        cv2.putText(image, str(image_name), (1150, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-    
     video.write(image)
+    cv2.imwrite(f'output/{filename}.jpg', image)
 
 video.release()

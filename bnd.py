4aa5158e978751f47e071367ac92111363a9f597
angelomatel <mateljohnangelo@gmail.com>
2024-09-21 17:12:22 +0800

    Update bnd.py
    Make loops faster
    Cover all returns

--- bnd.py
+++ bnd.py
@@ -1,12 +1,10 @@
 from shapely.geometry import Polygon
 import cv2
 import numpy as np
-import os
-import torch
 import json
 from glob import glob
 
-def check_overlap(coords, label, img):
+def check_overlap(coords, label, ped_status):
     """
     coords: array of coords of the object e.g. [x1, y1, x2, y2]
     label: provided label of the object
@@ -15,31 +13,45 @@ def check_overlap(coords, label, img):
     # Taken from CVAT.ai annotation tool
     roi_pedlane = [(46.80, 366.90), (17.30, 463.30), (199.67, 443.63), (342.79, 426.02), (720.40, 383.08), (1228.00, 333.00), (1194.90, 326.94), (1155.27, 320.33), (1092.51, 306.02), (1052.88, 296.11), (1031.96, 289.50), (680.70, 313.70), (687.38, 321.43), (692.88, 329.14), (692.90, 338.70), (681.50, 345.20), (664.80, 346.80), (647.70, 346.70), (625.90, 344.50), (607.30, 340.90), (590.20, 333.60), (570.30, 320.70), (326.27, 342.35)]
     roi_pedlane = Polygon(roi_pedlane)
-    X1, Y1, X2, Y2 = coords
-    new_coords = [(X1, Y1), (X2, Y1), (X2, Y2), (X1, Y2)]
-    object_polygon = Polygon(new_coords)
-
-    ped_status = get_light(img)
-
+    #print(f'roi_pedlane: {roi_pedlane}')
+    #print(f'coords: {coords}')
+    x1, y1, x2, y2 = coords
+    object_polygon = Polygon([(x1, y1), (x2, y1), (x2, y2), (x1, y2)])
+    #print(object_polygon)
     
     if label == 'Enforcer':
         return 'Enforcer'
+    
     elif label == 'Pedestrian' or label == 'Pedestrian-Violator':
         if ped_status == 'Red light':
             if roi_pedlane.intersection(object_polygon).area > 0.2 * object_polygon.area:
+                if label == 'Pedestrian':
+                    print('Converted to Pedestrian-Violator')
                 return 'Pedestrian-Violator'
+            else:
+                return 'Pedestrian'
         else:
             return 'Pedestrian'
+        
     elif label == 'Vehicle' or label == 'Vehicle-Violator':
         if ped_status == 'Green light':
             if roi_pedlane.intersection(object_polygon).area > 0.2 * object_polygon.area:
+                if label == 'Vehicle':
+                    print('Converted to Vehicle-Violator')
                 return 'Vehicle-Violator'
+            else:
+                return 'Vehicle'
         else:
             return 'Vehicle'
+        
     elif label == 'Bicycle' or label == 'Bicycle-Violator':
         if ped_status == 'Green light':
             if roi_pedlane.intersection(object_polygon).area > 0.2 * object_polygon.area:
+                if label == 'Bicycle':
+                    print('Converted to Bicycle-Violator')
                 return 'Bicycle-Violator'
+            else:
+                return 'Bicycle'
         else:
             return 'Bicycle'
 
@@ -66,16 +78,21 @@ def test():
     prediction_path = glob('pytorch_faster_rcnn_tutorial/data/heads/predictions/*.json')
     input_path = glob('pytorch_faster_rcnn_tutorial/data/heads/test/*.jpg')
 
-    for file in prediction_path:
-        for j in range(len(prediction_path)):
-            with open(file,'r') as f:
-                data = json.load(f)
-                for i in range(len(data['labels'])):
-                    data['labels'][i] = check_overlap(data['boxes'][i], data['labels'][i], input_path[j])
-                    print(data['boxes'][i])
+    for i in range(len(prediction_path)):
+        ped_status = get_light(input_path[i])
+        print(f'Processing {prediction_path[i]} | Light status: {ped_status}')
+        with open(prediction_path[i], 'r') as f:
+            data = json.load(f)
+            for j in range(len(data['labels'])):
+                result = check_overlap(data['boxes'][j], data['labels'][j], ped_status)
+                if result == None:
+                    print(f"{data['labels'][j]} is not a pedestrian or vehicle")
 
-            with open(file, 'w') as f:
-                json.dump(data, f, indent=4)
+                data['labels'][j] = result
+                # print(data['boxes'][i])
+
+        with open(prediction_path[i], 'w') as f:
+            json.dump(data, f, indent=4)
 
 if __name__ == "__main__":
     test()
